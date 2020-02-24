3daaafd6f8cd9c98d4f21052e3eee20ad840c9f7
alefemacedo <almacedobr2@gmail.com>
2020-02-24 12:21:29 -0300

    Novas rotas necessarias para a funcionalidade de locacao de livros

--- api/routes/web.php
+++ api/routes/web.php
@@ -10,7 +10,25 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+$router->group(['middleware' => 'cors'], function () use ($router) {
+    $router->get('/', function () use ($router) {
+        return $router->app->version();
+    });
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+    $router->group(['as' => 'user', 'prefix' => 'user'], function () use ($router) {
+        $router->get('/', ['as' => 'index', 'uses' => 'UserController@index']);
+        $router->post('/create', ['as' => 'create', 'uses' => 'UserController@create']);
+    });
+
+    $router->group(['as' => 'book', 'prefix' => 'book'], function () use ($router) {
+        $router->get('/', ['as' => 'index', 'uses' => 'BookController@index']);
+        $router->post('/create', ['as' => 'create', 'uses' => 'BookController@create']);
+    });
+
+    $router->group(['as' => 'receipt', 'prefix' => 'receipt'], function () use ($router) {
+        $router->get('/', ['as' => 'index', 'uses' => 'ReceiptController@index']);
+        $router->get('/fetch', ['as' => 'fetch', 'uses' => 'ReceiptController@fetch']);
+        $router->post('/create', ['as' => 'create', 'uses' => 'ReceiptController@create']);
+        $router->delete('/remove', ['as' => 'remove', 'uses' => 'ReceiptController@remove']);
+    });
 });
